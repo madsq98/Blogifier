56266fb03fefaba099eb2d90cb1e9f68000b0ac7
LindholmGithub <christianlindholm@jubii.dk>
2022-04-21 14:11:01 +0200

    Added 200ms Fail Test

--- k6/performanceTests.js
+++ k6/performanceTests.js
@@ -1,7 +1,13 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  thresholds: {
+      http_req_duration: ['p(95)<200'],
+  },
+};
+
 export default function () {
     http.get('http://vps.qvistgaard.me:9888/api/post/list/All/Post');
-    sleep(1);
+
 }
